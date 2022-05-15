5c0e68bd7661be93f01566e46e0a2857349698b6
Florian Klotz <klotz.f@googlemail.com>
2022-05-15 21:13:23 +0200

    add missing copy_object

--- lib/fog/backblaze/storage.rb
+++ lib/fog/backblaze/storage.rb
@@ -3,16 +3,17 @@ require 'digest'
 require 'cgi'
 
 class Fog::Backblaze::Storage < Fog::Service
-  #requires 
+  # requires
   recognizes :b2_account_id, :b2_account_token, :b2_key_id, :b2_key_token,
              :b2_bucket_name, :b2_bucket_id, :token_cache, :logger
 
   model_path 'fog/backblaze/storage/models'
   model       :directory
   collection  :directories
+  request     :copy_object
   model       :file
   collection  :files
 
-  autoload :Mock, File.expand_path("../storage/mock", __FILE__)
-  autoload :Real, File.expand_path("../storage/real", __FILE__)
+  autoload :Mock, File.expand_path('storage/mock', __dir__)
+  autoload :Real, File.expand_path('storage/real', __dir__)
 end
